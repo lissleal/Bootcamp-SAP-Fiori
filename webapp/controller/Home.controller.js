71a6e4e5255d5f29e214075e88ad8f773ec50e2d
Lissett Leal Naranjo <lissett777@gmail.com>
2025-03-13 16:52:32 +0000

    Input asociado a boton

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,10 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller", "sap/m/MessageBox", "sap/m/MessageToast"
+], function (Controller, MessageBox, MessageToast){
     "use strict";
 
     return Controller.extend("com.bootcamp.sapui5.freestyle.controller.Home", {
-        onInit() {
-        }
+
+        onPress: function () {
+            //Repasar esta parte
+            var oInput = this.getView().byId("nameInput");
+            var sValue = oInput.getValue();
+
+			MessageBox.show("Hola, "+ sValue);
+		}
+
     });
 });
