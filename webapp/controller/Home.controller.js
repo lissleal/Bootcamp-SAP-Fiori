605755c480e9d35940fda277650368baf6f09f8d
Lissett Leal Naranjo <lissett777@gmail.com>
2025-03-13 15:18:24 +0000

    button Say Hello y controlador Message Box

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,10 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller", "sap/m/MessageBox", "sap/m/MessageToast"
+], function (Controller, MessageBox, MessageToast){
     "use strict";
 
     return Controller.extend("com.bootcamp.sapui5.freestyle.controller.Home", {
-        onInit() {
-        }
+
+        onPress: function () {
+            //Repasar esta parte
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sText = oBundle.getText("textRespuestaBotoni18n");
+
+			MessageBox.show(sText);
+		}
+
     });
 });
+
