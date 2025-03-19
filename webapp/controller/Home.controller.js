a1650da24369d74af26c7792f9826d95257a84ed
Lissett Leal Naranjo <lissett777@gmail.com>
2025-03-19 00:18:13 +0000

    Agrega toolbar con input y logica en controlador home para busqueda por id

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "com/bootcamp/sapui5/freestyle/utils/HomeHelper",
-], (Controller, HomeHelper) => {
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, HomeHelper, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("com.bootcamp.sapui5.freestyle.controller.Home", {
@@ -12,12 +14,24 @@ sap.ui.define([
             let oDatos = await HomeHelper.getDataProducts();     
             await HomeHelper.setProductModel(this, oDatos[0].results);                      
         },
+        //Navega a vista detail
         onItemPress: function(oEvent){
             let oSource = oEvent.getSource();
             let oDatos = oSource.getBindingContext("ProductCollection").getObject();
             this.oRouter.navTo("detail", {
             ProductID: oDatos.ProductID
             });
+        },
+        onChange: async function(oEvent){
+            let oFilter = [];
+            let oSource = oEvent.getSource();
+            let oTable = this.getView().byId("idProductsTable");
+            let oBinding = oTable.getBinding("items");
+
+            if(oSource.getValue()){
+                oFilter = new Filter("ProductID", FilterOperator.EQ, oSource.getValue());
+            }
+            oBinding.filter(oFilter);
         }
     
     });
